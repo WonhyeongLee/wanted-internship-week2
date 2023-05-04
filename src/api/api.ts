9398e2dbf70dde443e69b1f771e6a3fb490eb195
WonhyeongLee <bigfile57@naver.com>
2023-05-04 21:29:05 +0900

    프록시 테스트

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,12 @@
 import axios from 'axios'
+
 const instance = axios.create({
   headers: { 'Content-Type': 'application/json' }
 })
 export const getSearchKeyword = async (keyword: string) => {
   try {
     const response = await instance.get(
-      `api/v1/search-conditions/?name=${keyword}`
+      `/api/v1/search-conditions/?name=${keyword}`
     )
     console.log(`api called`)
 
