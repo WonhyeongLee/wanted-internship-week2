304f1ab2080d90e7b27fbf4833abaa54eba7f3e6
WonhyeongLee <bigfile57@naver.com>
2023-05-04 21:54:42 +0900

    fix: api.ts에 프록시 설정 추가

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
-
+const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy'
 const instance = axios.create({
   headers: { 'Content-Type': 'application/json' }
 })
 export const getSearchKeyword = async (keyword: string) => {
   try {
     const response = await instance.get(
-      `/api/v1/search-conditions/?name=${keyword}`
+      `${PROXY}/api/v1/search-conditions/?name=${keyword}`
     )
     console.log(`api called`)
 
